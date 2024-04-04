60dd1c11e0cc6c95ab371344c92063db562b6e09
Abdul Hakim <hakim@undercurrent.tech>
2024-04-04 07:58:27 +0700

    update notification skeleton

--- src/components/Skeleton/NotificationSkeleton.tsx
+++ src/components/Skeleton/NotificationSkeleton.tsx
@@ -1,16 +1,28 @@
-import { ListItemIcon, ListItemText, MenuItem, Skeleton } from "@mui/material";
+import { ListItemIcon, ListItemText, MenuItem, MenuList, Paper, Skeleton } from "@mui/material";
 import { FC } from "react";
 
 const NotificationSkeleton: FC = () => {
   return (
-    <MenuItem style={{ whiteSpace: "normal", width: "420px" }}>
-      <ListItemIcon>
-        <Skeleton sx={{ bgcolor: "grey.800" }} variant="circular" width={30} height={30} />
-      </ListItemIcon>
-      <ListItemText>
-        <Skeleton sx={{ bgcolor: "grey.800" }} variant="rectangular" width={340} height={31} />
-      </ListItemText>
-    </MenuItem>
+    <Paper
+      sx={{
+        width: 320,
+        maxWidth: "100%",
+        margin: 1,
+        backgroundColor: "#1d1d3b",
+      }}
+    >
+      <MenuList>
+        <MenuItem style={{ whiteSpace: "normal" }}>
+          <ListItemIcon>
+            <Skeleton sx={{ bgcolor: "grey.800" }} variant="circular" width={30} height={30} />
+          </ListItemIcon>
+          <ListItemText>
+            <Skeleton sx={{ bgcolor: "grey.800" }} variant="rectangular" width="100%" height={31} />
+            <Skeleton sx={{ bgcolor: "grey.800", marginTop: "5px" }} variant="rectangular" width="20%" height={10} />
+          </ListItemText>
+        </MenuItem>
+      </MenuList>
+    </Paper>
   );
 };
 
